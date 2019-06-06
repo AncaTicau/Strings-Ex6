731fe596e1fc31e2dac0fafebef86ecb733405ea
MariusA <anca.ticau@gmail.com>
2019-06-06 23:01:47 +0300

    Exercise 6

---
+++ Strings-Ex6/Strings-Ex6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Strings_Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = FormatNumber(54.23D);
+
+            Console.WriteLine(" The percentage is : {0}.", result.ToString("P", CultureInfo.InvariantCulture));
+        }
+
+        //Write a method that formats a number with a percentage
+
+        public static double FormatNumber(double num)
+        {
+            double percentage = num / 100;
+
+            return percentage;
+        }
+
+    }
+}
